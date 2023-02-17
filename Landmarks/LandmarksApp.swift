cf00a57a11ccc16f6eccdb2e18ce288d9963c5e9
mercvre <jordan_louis@outlook.com>
2023-02-18 00:01:22 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Heesham on 2/18/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
