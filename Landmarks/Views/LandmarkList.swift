a33d0ff70811da8e708b1cbfea2bb401fa93d85e
mercvre <jordan_louis@outlook.com>
2023-02-27 03:00:00 +0100

    First commit

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,37 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Heesham on 2/18/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        
+        NavigationView {
+            // List(landmarks, id: \.id) { landmark in
+            List(landmarks) { landmark in //identifiable protocol
+                NavigationLink {
+                    LandmarkDetail(landmark: landmark)
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+                
+            }
+            .navigationTitle("Landmarks")
+        }
+        
+
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        ForEach(["iPhone SE (2nd generation)", "iPhone Xs Max"], id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+        }
+    }
+}
